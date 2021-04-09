58c1b6923c3e47c73048f644e1debcf9bd3b8378
unknown <imbrices@gmail.com>
2021-04-09 19:59:11 -0300

    Primer

---
+++ Colectivos_Posiciones/Colectivos_Posiciones/Controllers/BaseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Colectivos_Posiciones.Controllers
+{
+    public class BaseController : Controller
+    {
+        private String baseURL = "https://apitransporte.buenosaires.gob.ar/colectivos/";      
+
+        internal IRestResponse TraerDatos(String client_secret, String client_id)
+        {
+            String url = baseURL + "vehiclePositionsSimple?client_secret=" + client_secret + "&client_id=" + client_id;
+            var client = new RestClient(url);
+            client.Timeout = -1;
+            var request = new RestRequest(Method.GET);
+            request.AddHeader("Cookie", "BIGipServerPool_apitransporte-BSAS-prd=2936017930.39455.0000; BIGipServerPool_apitransporte-prd=rd1o00000000000000000000ffff0a0a0560o8090");
+            IRestResponse response = client.Execute(request);
+            return response;
+        }
+    }
+}
---
+++ Colectivos_Posiciones/Colectivos_Posiciones/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Colectivos_Posiciones.Models;
+using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Colectivos_Posiciones.Controllers
+{
+    public class HomeController : BaseController
+    {
+        private String api_key_google_maps = "XXXXXX";
+        private String client_secret_api_gcba= "XXXXXX";
+        private String client_id_api_gcba = "XXXXXX";
+        
+        public ActionResult Index()
+        {            
+            ViewBag.API_KEY = api_key_google_maps; 
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult VerColectivos()
+        {
+            IRestResponse colectivos = TraerDatos(client_secret_api_gcba, client_id_api_gcba);
+            return Json(colectivos.Content);
+        }
+    }
+}
